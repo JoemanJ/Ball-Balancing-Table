5987bcd76bbd2937e25a0ce3ecc2f7f0ee1a1a4d
JoemanJ <diogodelazare@hotmail.com>
2025-03-14 23:04:40 -0300

    ready to test camera

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,49 @@
 #include <Arduino.h>
 #include "menu.h"
-// #include "camera.h"
+#include "camera.h"
+#include "display.h"
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Serial begun");
-  setupMainMenu();
+  // setupMainMenu();
   // setupCamera();
   sei();
 }
 
 void loop() {
-  // switch(updateBallPosition())
-  // {
-  //   case REQUEST_FAIL:
-  //     Serial.println("Failed to request object data");
-  //     Serial.println("Check camera connection");
-  //     break;
+  switch(updateBallPosition())
+  {
+    case REQUEST_FAIL:
+      // Serial.println("Failed to request object data");
+      // Serial.println("Check camera connection");
+      displayDebugText(L1, "REQUEST FAIL");
+      break;
 
-  //   case NOT_LEARNED:
-  //     Serial.println("Camera has not learned an object");
-  //     Serial.println("Learn an object first");
-  //     break;
+    case NOT_LEARNED:
+      // Serial.println("Camera has not learned an object");
+      // Serial.println("Learn an object first");
+      displayDebugText(L1, "NOT LEARNED");
+      break;
 
-  //   case NO_OBJECT:
-  //     Serial.println("No object found");
-  //     break;
+    case NO_OBJECT:
+      // Serial.println("No object found");
+      displayDebugText(L1, "NO OBJECT");
+      break;
 
-  //   case UPDATE_SUCCESS:
-  //     Serial.print("X:");
-  //     Serial.print(ballX);
-  //     Serial.print(" Y:");
-  //     Serial.println(ballY);
-  //     break;
+    case UPDATE_SUCCESS:
+      // Serial.print("X:");
+      // Serial.print(ballX);
+      // Serial.print(" Y:");
+      // Serial.println(ballY);
+      displayDebugInt(L1, ballX, "X: ");
+      displayDebugInt(L2, ballY, "Y: ");
+      break;
 
-  //   default:
-  //     Serial.println("Unknown error!");
-  // }
+    default:
+      // Serial.println("Unknown error!");
+      displayDebugText(L1, "UNKNOWN ERROR");
+  }
 
-  // delay(1000);
+  delay(1000);
 }
