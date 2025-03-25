5989f57f25c1991b4aeb3eb63f2f0fa4d1338eb9
JoemanJ <diogodelazare@hotmail.com>
2025-03-24 22:05:46 -0300

    added menus for multiple coefs

--- src/menu.cpp
+++ src/menu.cpp
@@ -24,12 +24,12 @@ void changeMenu(Menu menu)
     changeScreen(menu.displayFunc);
 }
 
-
 #define P_COLUMN 0
 #define I_COLUMN 6
 #define D_COLUMN 12
 
-uint8_t cursorColumn = P_COLUMN;
+enum CURSORPOSITION {P, I, D, P_I, I_D, D_P, P_I_D};
+CURSORPOSITION cursorPosition = P;
 
 static void mainMenuOnLeft();
 static void mainMenuOnRight();
@@ -83,37 +83,140 @@ Menu derMenu = {
     }
 };
 
+static void proIntMenuOnLeft();
+static void proIntMenuOnRight();
+static void proIntMenuOnPress();
+static void proIntMenuDisplay();
+Menu proIntMenu = {
+    .displayFunc = proIntMenuDisplay,
+    .controls = {
+        .onLeft = proIntMenuOnLeft,
+        .onRight = proIntMenuOnRight,
+        .onPress = proIntMenuOnPress,
+    }
+};
+
+static void intDerMenuOnLeft();
+static void intDerMenuOnRight();
+static void intDerMenuOnPress();
+static void intDerMenuDisplay();
+Menu intDerMenu = {
+    .displayFunc = intDerMenuDisplay,
+    .controls = {
+        .onLeft = intDerMenuOnLeft,
+        .onRight = intDerMenuOnRight,
+        .onPress = intDerMenuOnPress,
+    }
+};
+
+static void derProMenuOnLeft();
+static void derProMenuOnRight();
+static void derProMenuOnPress();
+static void derProMenuDisplay();
+Menu derProMenu = {
+    .displayFunc = derProMenuDisplay,
+    .controls = {
+        .onLeft = derProMenuOnLeft,
+        .onRight = derProMenuOnRight,
+        .onPress = derProMenuOnPress,
+    }
+};
+
+static void proIntDerMenuOnLeft();
+static void proIntDerMenuOnRight();
+static void proIntDerMenuOnPress();
+static void proIntDerMenuDisplay();
+Menu proIntDerMenu = {
+    .displayFunc = proIntDerMenuDisplay,
+    .controls = {
+        .onLeft = proIntDerMenuOnLeft,
+        .onRight = proIntDerMenuOnRight,
+        .onPress = proIntDerMenuOnPress,
+    }
+};
+
 void mainMenuDisplay()
 {
-    displaySetCursor(0, 0);
-    displayWrite("PRO:  INT:  DER:");
-
     displaySetCursor(0, 1);
     displayWrite(getPCoef());
     displaySetCursor(6, 1);
     displayWrite(getICoef());
     displaySetCursor(12, 1);
     displayWrite(getDCoef());
+    
+    displaySetCursor(0, 0);
+    switch (cursorPosition)
+    {
+    case P_I:
+        displayWrite("PRO:<>INT:  DER:");
+        displayNoCursor();
+        break;
+    
+    case I_D:
+        displayWrite("PRO:  INT:<>DER:");
+        displayNoCursor();
+        break;
 
-    displaySetCursor(cursorColumn, 0);
-    displayCursor();
+    case D_P:
+        displayWrite("PRO:< INT: >DER:");
+        displayNoCursor();
+        break;
+
+    case P_I_D:
+        displayWrite("PRO:<>INT:<>DER:");
+        displayNoCursor();
+    
+    case P:
+        displayWrite("PRO:  INT:  DER:");
+        displaySetCursor(P_COLUMN, 0);
+        displayCursor();
+        break;
+
+    case I:
+        displayWrite("PRO:  INT:  DER:");
+        displaySetCursor(I_COLUMN, 0);
+        displayCursor();
+        break;
+
+    case D:
+        displayWrite("PRO:  INT:  DER:");
+        displaySetCursor(D_COLUMN, 0);
+        displayCursor();
+        break;
+
+    default:
+        break;
+    }
+
     displayNoBlink();
 }
 
 void mainMenuOnLeft()
 {
-    switch(cursorColumn)
+    switch(cursorPosition)
     {
-        case P_COLUMN:
-            cursorColumn = D_COLUMN;
+        case P:
+            cursorPosition = P_I_D;
             break;
 
-        case I_COLUMN:
-            cursorColumn = P_COLUMN;
+        case I:
+            cursorPosition = P;
             break;
 
-        case D_COLUMN:
-            cursorColumn = I_COLUMN;
+        case D:
+            cursorPosition = I;
+            break;
+
+        case P_I:
+            cursorPosition = D;
+            break;
+
+        case I_D:
+            cursorPosition = P_I;
+            break;
+
+        case P_I_D:
+            cursorPosition = I_D;
             break;
 
         default:
@@ -125,18 +228,30 @@ void mainMenuOnLeft()
 
 void mainMenuOnRight()
 {
-    switch(cursorColumn)
+    switch(cursorPosition)
     {
-        case P_COLUMN:
-            cursorColumn = I_COLUMN;
+        case P:
+            cursorPosition = I;
             break;
 
-        case I_COLUMN:
-            cursorColumn = D_COLUMN;
+        case I:
+            cursorPosition = D;
             break;
 
-        case D_COLUMN:
-            cursorColumn = P_COLUMN;
+        case D:
+            cursorPosition = P_I;
+            break;
+
+        case P_I:
+            cursorPosition = I_D;
+            break;
+
+        case I_D:
+            cursorPosition = P_I_D;
+            break;
+
+        case P_I_D:
+            cursorPosition = P;
             break;
 
         default:
@@ -148,18 +263,34 @@ void mainMenuOnRight()
 
 void mainMenuOnPress()
 {
-    switch(cursorColumn)
+    switch(cursorPosition)
     {
-        case P_COLUMN:
+        case P:
             changeMenu(proMenu);
             break;
 
-        case I_COLUMN:
+        case I:
             changeMenu(intMenu);
             break;
 
-        case D_COLUMN:
+        case D:
             changeMenu(derMenu);
+            break;
+        
+        case P_I:
+            changeMenu(proIntMenu);
+            break;
+
+        case I_D:
+            changeMenu(intDerMenu);
+            break;
+
+        case D_P:
+            changeMenu(derProMenu);
+            break;
+
+        case P_I_D:
+            changeMenu(proIntDerMenu);
             break;
 
         default:
@@ -171,17 +302,18 @@ void mainMenuOnPress()
 
 
 
+
 void proMenuDisplay()
 {
     displaySetCursor(0, 0);
     displayWrite("PRO:  INT:  DER:");
 
-    displaySetCursor(0, 1);
+    displaySetCursor(P_COLUMN, 1);
     displayWrite(getPCoef());
     displayWrite("   ");
-    displaySetCursor(6, 1);
+    displaySetCursor(I_COLUMN, 1);
     displayWrite(getICoef());
-    displaySetCursor(12, 1);
+    displaySetCursor(D_COLUMN, 1);
     displayWrite(getDCoef());
 
     displaySetCursor(P_COLUMN, 1);
@@ -214,12 +346,12 @@ void intMenuDisplay()
     displaySetCursor(0, 0);
     displayWrite("PRO:  INT:  DER:");
 
-    displaySetCursor(0, 1);
+    displaySetCursor(P_COLUMN, 1);
     displayWrite(getPCoef());
-    displaySetCursor(6, 1);
+    displaySetCursor(I_COLUMN, 1);
     displayWrite(getICoef());
     displayWrite("   ");
-    displaySetCursor(12, 1);
+    displaySetCursor(D_COLUMN, 1);
     displayWrite(getDCoef());
 
     displaySetCursor(I_COLUMN, 1);
@@ -252,11 +384,11 @@ void derMenuDisplay()
     displaySetCursor(0, 0);
     displayWrite("PRO:  INT:  DER:");
 
-    displaySetCursor(0, 1);
+    displaySetCursor(P_COLUMN, 1);
     displayWrite(getPCoef());
-    displaySetCursor(6, 1);
+    displaySetCursor(I_COLUMN, 1);
     displayWrite(getICoef());
-    displaySetCursor(12, 1);
+    displaySetCursor(D_COLUMN, 1);
     displayWrite(getDCoef());
     displayWrite("   ");
 
@@ -282,6 +414,177 @@ void derMenuOnPress()
     changeMenu(mainMenu);
     refreshDisplay();
 }
+
+
+
+void proIntMenuDisplay()
+{
+    displaySetCursor(0, 0);
+    displayWrite("PRO:  INT:  DER:");
+
+    displaySetCursor(P_COLUMN, 1);
+    displayWrite(getPCoef());
+    displayWrite("   ");
+    displaySetCursor(I_COLUMN-2, 1);
+    displayWrite("<>");
+    displayWrite(getICoef());
+    displayWrite("   ");
+    displaySetCursor(D_COLUMN, 1);
+    displayWrite(getDCoef());
+
+    displayNoCursor();
+    displayNoBlink();
+}
+
+void proIntMenuOnLeft()
+{
+    decPCoef();
+    decICoef();
+    refreshDisplay();
+}
+
+void proIntMenuOnRight()
+{
+    incPCoef();
+    incICoef();
+    refreshDisplay();
+}
+
+void proIntMenuOnPress()
+{
+    changeMenu(mainMenu);
+    refreshDisplay();
+}
+
+
+
+void intDerMenuDisplay()
+{
+    displaySetCursor(0, 0);
+    displayWrite("PRO:  INT:  DER:");
+
+    displaySetCursor(P_COLUMN, 1);
+    displayWrite(getPCoef());
+    displaySetCursor(I_COLUMN, 1);
+    displayWrite(getICoef());
+    displayWrite("   ");
+    displaySetCursor(D_COLUMN-2, 1);
+    displayWrite("<>");
+    displayWrite(getDCoef());
+    displayWrite("   ");
+
+    displayNoCursor();
+    displayNoBlink();
+}
+
+void intDerMenuOnLeft()
+{
+    decICoef();
+    decDCoef();
+    refreshDisplay();
+}
+
+void intDerMenuOnRight()
+{
+    incICoef();
+    incDCoef();
+    refreshDisplay();
+}
+
+void intDerMenuOnPress()
+{
+    changeMenu(mainMenu);
+    refreshDisplay();
+}
+
+
+
+void derProMenuDisplay()
+{
+    displaySetCursor(0, 0);
+    displayWrite("PRO:  INT:  DER:");
+
+    displaySetCursor(P_COLUMN, 1);
+    displayWrite(getPCoef());
+    displayWrite("   ");
+    displaySetCursor(I_COLUMN-2, 1);
+    displayWrite("< ");
+    displayWrite(getICoef());
+    displaySetCursor(D_COLUMN-2, 1);
+    displayWrite(" >");
+    displayWrite(getDCoef());
+    displayWrite("   ");
+
+    displayNoCursor();
+    displayNoBlink();
+}
+
+void derProMenuOnLeft()
+{
+    decDCoef();
+    decPCoef();
+    refreshDisplay();
+}
+
+void derProMenuOnRight()
+{
+    incDCoef();
+    incPCoef();
+    refreshDisplay();
+}
+
+void derProMenuOnPress()
+{
+    changeMenu(mainMenu);
+    refreshDisplay();
+}
+
+
+
+void proIntDerMenuDisplay()
+{
+    displaySetCursor(0, 0);
+    displayWrite("PRO:  INT:  DER:");
+
+    displaySetCursor(P_COLUMN, 1);
+    displayWrite(getPCoef());
+    displayWrite("   ");
+    displaySetCursor(I_COLUMN-2, 1);
+    displayWrite("<>");
+    displayWrite(getICoef());
+    displayWrite("   ");
+    displaySetCursor(D_COLUMN-2, 1);
+    displayWrite("<>");
+    displayWrite(getDCoef());
+    displayWrite("   ");
+
+    displayNoCursor();
+    displayNoBlink();
+}
+
+void proIntDerMenuOnLeft()
+{
+    decDCoef();
+    decICoef();
+    decPCoef();
+    refreshDisplay();
+}
+
+void proIntDerMenuOnRight()
+{
+    incDCoef();
+    incICoef();
+    incPCoef();
+    refreshDisplay();
+}
+
+void proIntDerMenuOnPress()
+{
+    changeMenu(mainMenu);
+    refreshDisplay();
+}
+
+
 
 void setupMainMenu()
 {
